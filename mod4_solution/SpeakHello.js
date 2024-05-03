62220e419e7caca481fbbc67dca958697a36abe5
maitydishika2005 <152094479+maitydishika2005@users.noreply.github.com>
2024-05-03 22:57:02 +0530

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -5,6 +5,7 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
+(function (){
 var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
@@ -22,3 +23,4 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
  window.helloSpeaker = helloSpeaker;
+})();
